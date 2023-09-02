b0cc6212d2ee21bde879008a3d3def9e107049b5
Aysegul ESKI <ayseguleskk@gmail.com>
2023-09-02 20:11:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //While
+        //1'den baslayarak console'dan girilen sayiya kadar(sayilar dahil) ortalama hesaplayip console'a yazdiran program
+
+        Console.WriteLine("Lutfen bir sayi giriniz:");
+        int sayi=int.Parse(Console.ReadLine());
+        int sayac=1;
+        int toplam=0;
+
+        while (sayac <= sayi)
+        {
+            toplam += sayac;
+            sayac++;   
+        }
+
+        double ortalama=toplam/sayi;        
+        Console.WriteLine(ortalama);
+
+        // a'dan z'ye kadar tum harfleri console'a yazdiran program
+        char karakter='a';
+        while (karakter <= 'z')
+        {
+            Console.WriteLine(karakter);
+            karakter++;
+        }
+
+        //ForEach
+
+        string[] arabalar={"Volkswagen","Ford","Toyota","Nissan"};
+        foreach (var araba in arabalar)
+        {
+           Console.WriteLine(araba); 
+        }
+
+    }
+}
